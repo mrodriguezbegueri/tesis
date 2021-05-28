838b08d78482608a4f7e4e034b89db8f8802c7af
Matias <matiasrodriguez@unsj-cuim.edu.ar>
2021-05-28 19:41:45 -0300

    Guardar respuestas terminado en NodeJs

--- nodeJS/results/handler.js
+++ nodeJS/results/handler.js
@@ -16,7 +16,7 @@ const response = (statusCode, message) => {
         body: JSON.stringify(message)
     };
   }
-
+// 
 // Add one result
 module.exports.addResult = async (event, context, callback) => {
     const reqBody = JSON.parse(event.body);
@@ -31,20 +31,19 @@ module.exports.addResult = async (event, context, callback) => {
         questions.forEach(q => {
           let answerId = uuidv4();
 
-            let answer = {
-                PutRequest: {
-                    Item: {
-                        PK: pollId,
-                        SK: answersId + '#' + answerId,
-                        type: q.type,
-                        label: q.label,
-                        value: q.value
-                    }
-                }
-            };
+          let answer = {
+            PutRequest: {
+              Item: {
+                PK: pollId,
+                SK: answersId + '#' + answerId,
+                type: q.type,
+                label: q.label,
+                value: q.value
+              }
+            }
+          };
             answers.push({...answer});
-        })
-    
+        });
     });
 
     const resultId = uuidv4();
@@ -58,6 +57,7 @@ module.exports.addResult = async (event, context, callback) => {
       createdAt: new Date().toISOString()
     };
   
+    // Save poll
    let res = await db.put({TableName: table, Item: result}).promise().catch(err => {
       return callback(null, response(err.statusCode, err));
    });
@@ -69,12 +69,11 @@ module.exports.addResult = async (event, context, callback) => {
             [table]: answers
           }
       }
-
       res = await db.batchWrite(params).promise().catch(err => {
         return callback(null, response(err.statusCode, err))
       });
-
       return callback(null, response(200, res));
+   } else {
+      return callback(null, 'No se puedo crear la encuesta');
    }
-   
   }
