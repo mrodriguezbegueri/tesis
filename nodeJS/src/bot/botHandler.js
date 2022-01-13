677d70114bbdbc200f717fc7fb9c74eae5f33fad
Matias <mrodriguez@folcode.com>
2022-01-13 10:16:33 -0300

    avances bot

--- nodeJS/src/bot/botHandler.js
+++ nodeJS/src/bot/botHandler.js
@@ -14,27 +14,51 @@ const response = (statusCode, message) => {
 
 const run = async (event) => {
 
-    // Obtener la encuesta
-
     try {
-
+        // Obtener la encuesta        
         const pollTitle = event.queryStringParameters.title
         console.log('pollTitle', pollTitle)
 
-        const getPollRequest = await axios.get(APIG_URL + '/polls/getPollByTitle', {
-            params: {
-                title: pollTitle
-            }
-        })
+        const params = {
+            title: pollTitle
+        }
+
+        const getPollRequest = await axios.get(APIG_URL + '/polls/getPollByTitle', { params })
     
         const poll = getPollRequest.data
         console.log('poll', poll)
-    
+
+        // Armar las respuestas
+        getPollResult(poll)
+        console.log('poll after: ', JSON.stringify(poll))
+        
+        
         return response(200, 'ok')
 
     } catch(err) {
         console.log('err', err)
     }
+}
+
+const getPollResult = (poll) => {
+    poll.groups.forEach(group => {
+        group.questions.forEach(question => {
+            addRandomAnswer(question)
+        })
+    })
+}
+
+const addRandomAnswer = (question) => {
+    const contOptions = question.options.length
+    const randomIndex = getRandomInt(0, (contOptions - 1))
+    const answer = question.options[randomIndex].label
+    question.value = answer 
+}
+
+const getRandomInt = (min, max) => {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 module.exports = {
