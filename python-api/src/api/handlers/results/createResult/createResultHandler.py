a411a05e31f52eb083e3d4ddc781c09ad3e2de10
Matias <mrodriguez@folcode.com>
2022-04-14 17:18:12 -0300

    createResult api-python

---
+++ python-api/src/api/handlers/results/createResult/createResultHandler.py
@@ -0,0 +1,43 @@
+try:
+    import unzip_requirements
+except ImportError:
+    pass
+
+import boto3
+import os
+import simplejson as json
+
+dynamodb_client = boto3.resource('dynamodb')
+TABLE_NAME = os.environ['POLLS_TABLE_NAME']
+
+def create_response(code, message):
+  return {
+    'statusCode': code,
+    'headers': {'Content-Type': 'application/json'},
+    'body': json.dumps(message)
+  }
+
+def createResult(event, context):
+
+    result = json.loads(event['body'])
+
+    params = {
+      'Item': result
+    }
+
+    print('params', params)
+
+    try:
+      table = dynamodb_client.Table(TABLE_NAME)
+      response = table.put_item(**params)
+    except Exception as ex:
+      print('Ex: ', ex)
+      return create_response(500, 'Error creating the Result')
+
+    print('Response', response)
+
+    if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+      return create_response(500, 'Error creating the Result')
+
+    return create_response(200, params['Item'])
+
